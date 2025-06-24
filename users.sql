2cac01d9e6d003d958a902ce0a58f92b189e8d99
Viktoriia <banah.vaiktoria@icloud.com>
2025-06-24 15:21:49 +0300

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
